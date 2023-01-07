2dd1c3fca7e4580458911378a4560dbfce42b0bf
upsydedown <chintansenapati@gmail.com>
2023-01-08 04:25:47 +0530

    Adding the CSS

--- src/Components/To-Do-Webapp/todoapp.js
+++ src/Components/To-Do-Webapp/todoapp.js
@@ -53,9 +53,6 @@ const Todoapp = () => {
         settoggleButton(true);
     }
 
-
-
-
     // Delete Item function
 
     function deleteItems(index) {
@@ -64,7 +61,6 @@ const Todoapp = () => {
         });
         setItems(updatedItems);
     }
-
 
     // Remove All Item function
 
@@ -81,37 +77,37 @@ const Todoapp = () => {
     return (
         <>
             <div className="main-body">
-                <div className="image-body"></div>
-                <figure>
-                    <img src="" alt="" srcSet="" />
-                    <figcaption>Lorem ipsum dolor sit amet.</figcaption>
-                </figure>
-                <div className="addItems">
-                    <input type="text" className='form-control' placeholder='Enter item name' value={inputData} onChange={(event) => setinputData(event.target.value)} />
-                    {toggleButton ? (<i className="fa fa-pencil edit-btn" aria-hidden="true" onClick={() => addItems()}></i>) : (<i className="fa fa-plus add-btn" aria-hidden="true" onClick={() => addItems()}></i>)}
-                </div>
+                <div className="content-box container">
+                    <div className="image-body"></div>
+                    <figure className='image-box container'>
+                        <img className='logo' src="images/logo.png" alt="" srcSet="" />
+                    </figure>
+                    <div className="addItems">
+                        <input type="text" className='form-control' placeholder='Enter item name' value={inputData} onChange={(event) => setinputData(event.target.value)} />
+                        {toggleButton ? (<i className="fa fa-pencil edit-btn" aria-hidden="true" onClick={() => addItems()}></i>) : (<i className="fa fa-plus add-btn" aria-hidden="true" onClick={() => addItems()}></i>)}
+                    </div>
 
-                {/* Show Items Section */}
+                    {/* Show Items Section */}
 
-                <div className="item-list">
-
-                    {items.map((curElem, index) => {
-                        return (
-                            <div className="items" key={index}>
-                                <h3>{curElem.name}</h3>
-                                <div className="icon-btn">
-                                    <i className="fa fa-pencil edit-btn" aria-hidden="true" onClick={() => editItems(curElem.id)}></i>
-                                    <i className="fa fa-trash-alt trash-btn" aria-hidden="true" onClick={() => deleteItems(curElem.id)}></i>
+                    <div className="item-list container">
+                        {items.map((curElem, index) => {
+                            return (
+                                <div className="items" key={index}>
+                                    <h3 className='text'>{curElem.name}</h3>
+                                    <div className="icon-btn">
+                                        <i className="fa fa-pencil edit-btn" aria-hidden="true" onClick={() => editItems(curElem.id)}></i>
+                                        <i className="fa fa-trash-alt trash-btn" aria-hidden="true" onClick={() => deleteItems(curElem.id)}></i>
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    })}
-                </div>
+                            )
+                        })}
+                    </div>
 
-                <div className="show-item">
-                    <button className="show-btn" data-sm-link-text="Remove Items" onClick={RemoveAll}>
-                        <span>Show List</span>
-                    </button>
+                    <div className="show-item">
+                        <button className="show-btn" data-sm-link-text="Remove Items" onClick={RemoveAll}>
+                            <span>Show List</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
